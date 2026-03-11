4c19f91d0a8604d3eda2a4f35c102f8caf7da668
erdiballikaya <79500217+erdiballikaya@users.noreply.github.com>
2026-03-11 19:46:36 +0300

    Video model for OTT Video Demo

---
+++ OttVideoDemo/OttVideoDemo/Models/Video.cs
@@ -0,0 +1,10 @@
+public class Video
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ThumbnailUrl { get; set; }
+    public string VideoUrl { get; set; }
+    public int CategoryId { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
